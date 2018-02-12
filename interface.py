630fb204defd5b4e0733b4a2f0ddb91e57de0fc7
bc952813 <bc952813@ohio.edu>
2018-02-12 01:41:02 -0500

    Add files via upload
    Fixed syntax in the character list.

--- interface.py
+++ interface.py
@@ -7,7 +7,7 @@ disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 Selected_Character  = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U"]
 Selected_Character += ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","i","o","q","r","s","t","u"]
 Selected_Character += ["V","W","X","Y","Z",1,2,3,4,5,6,7,8,9,0,"_",".","/"]
-Selected_Character += ["v","w","x","y","z","!","@","#","$","%","^","&","*","(",")","\","<",">"]
+Selected_Character += ["v","w","x","y","z","!","@","#","$","%","^","&","*","(",")","\\","<",">"]
 
 d_up = 29                       #direction pad up
 d_down = 31                     #direction pad down
