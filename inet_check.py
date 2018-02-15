c2ca80e15693dc3d38bb0b92a76d5ab4081eb9a1
HolyShif <kyledshiflett@hotmail.com>
2018-02-14 23:17:57 -0500

    Fixed comment

--- inet_check.py
+++ inet_check.py
@@ -6,6 +6,7 @@ def check_connection(hostname):
     try:
         #do DNS lookup for hostname
         host = socket.gethostbyname(hostname)
+        #attempt to make TCP connection port 80
         skt = socket.create_connection((host,80),2)
         print "successful connection to " + SERVER + "\n"
         return True
