7624073acce2bd75be2045c77a1a7e28f68ff513
bc952813 <bc952813@ohio.edu>
2018-02-19 01:13:07 -0500

    Add files via upload

--- inet_check.py
+++ inet_check.py
@@ -8,12 +8,12 @@ def check_connection(hostname):
         host = socket.gethostbyname(hostname)
         #attempt to make TCP connection port 80
         skt = socket.create_connection((host,80),2)
-        print "successful connection to " + SERVER + "\n"
+        print ("successful connection to " + SERVER + "\n")
         return True
     except:
         pass
-    print "failed connection to " + SERVER + "\n"
+    print ("failed connection to " + SERVER + "\n")
     return False
 
 inet_connected = check_connection(SERVER)
-print inet_connected
+print (inet_connected)
