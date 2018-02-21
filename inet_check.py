03a451adbe65196711a0311c2a8607f307770426
HolyShif <kyledshiflett@hotmail.com>
2018-02-21 15:43:51 -0500

    Added unit test for inet_check

--- inet_check.py
+++ inet_check.py
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
+import socket
+
+def check_connection(hostname):
+    try:
+        #do DNS lookup for hostname
+        host = socket.gethostbyname(hostname)
+        #attempt to make TCP connection port 80
+        skt = socket.create_connection((host,80),2)
+        #print "successful connection to " + SERVER + "\n"
+        return True
+    except:
+        #pass
+        #print "failed connection to " + SERVER + "\n"
+        return False
+=======
 import socket
 
 SERVER = "www.adafruit.io"
@@ -8,12 +24,13 @@ def check_connection(hostname):
         host = socket.gethostbyname(hostname)
         #attempt to make TCP connection port 80
         skt = socket.create_connection((host,80),2)
-        print "successful connection to " + SERVER + "\n"
+        print ("successful connection to " + SERVER + "\n")
         return True
     except:
         pass
-    print "failed connection to " + SERVER + "\n"
+    print ("failed connection to " + SERVER + "\n")
     return False
 
 inet_connected = check_connection(SERVER)
-print inet_connected
+print (inet_connected)
+>>>>>>> 0c88aac7d5101ab5aa986cd537e4a99e629d167f
---
+++ test_net.py
@@ -0,0 +1,24 @@
+import unittest
+import inet_check
+
+class TestNetOn(unittest.TestCase):
+
+    def test_google(self):
+        self.assertTrue(inet_check.check_connection("www.google.com"))
+    def test_yahoo(self):
+        self.assertTrue(inet_check.check_connection("www.yahoo.com"))
+    def test_adafruitio(self):
+        self.assertTrue(inet_check.check_connection("www.adafruit.io"))
+
+class TestNetOff(unittest.TestCase):
+
+    def test_google(self):
+        self.assertFalse(inet_check.check_connection("www.google.com"))
+    def test_yahoo(self):
+        self.assertFalse(inet_check.check_connection("www.yahoo.com"))
+    def test_adafruitio(self):
+        self.assertFalse(inet_check.check_connection("www.adafruit.io"))
+
+
+if __name__ == '__main__':
+    unittest.main()
