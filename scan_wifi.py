6f00474cf7f6328c272847803215c75c804cbe70
HolyShif <kyledshiflett@hotmail.com>
2018-03-05 17:41:25 -0500

    cleaned up wifi scan

--- scan_wifi.py
+++ scan_wifi.py
@@ -2,15 +2,13 @@ import subprocess
 
 p1 = subprocess.Popen(["iwlist","wlan0","scan"],stdout=subprocess.PIPE)
 p2 = subprocess.Popen(["grep", "-oP", "SSID:\"\K[^\"]+"],stdin=p1.stdout,stdout=subprocess.PIPE)
-#p2 = subprocess.Popen(["grep", "SSID:"],stdin=p1.stdout,stdout=subprocess.PIPE)
 
-#p1.stdout.close()
-#out,err = p2.communicate()
+p1.stdout.close()
 out,err = p2.communicate()
-print(out)
+#print(out)
 essid = out.splitlines()
-print(essid)
+#print(essid)
 essid = list(set(essid))
-print(essid)
-print(len(essid))
+#print(essid)
+#print(len(essid))
 #sudo iwlist wlan0 scan | grep -oP 'SSID:"\K[^"]+'
